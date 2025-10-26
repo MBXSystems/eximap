cc372ffa86c8975f7e5fc5e951226f60595f16dd
DJ Jain <dj.jain@ahead.com>
2025-10-26 10:01:44 -0500

    Update socket to be more flexible when working with sockets
    With Erlang 28, socket appears to be a tuple with many more values.
    This commit updates the socket module to be more flexible with the
    various socket definitions by avoiding pattern matching.

--- lib/eximap/socket.ex
+++ lib/eximap/socket.ex
@@ -16,22 +16,45 @@ defmodule Eximap.Socket do
   @doc """
   Set options for the socket based on the type of the connection
   """
-  def setopts({:sslsocket, _, _} = socket, opts), do: :ssl.setopts(socket, opts)
-  def setopts({:gen_tcp, _, _, _} = socket, opts), do: :inet.setopts(socket, opts)
+  def setopts(socket, opts) do
+    socket
+    |> elem(0)
+    |> case do
+      :sslsocket -> :ssl.setopts(socket, opts)
+      :gen_tcp -> :inet.setopts(socket, opts)
+    end
+  end
 
   @doc """
   Send some data to the socket abstracting the type of the socket away
   """
-  def send({:sslsocket, _, _} = socket, msg), do: :ssl.send(socket, msg)
-  def send({:gen_tcp, _, _, _} = socket, msg), do: :gen_tcp.send(socket, msg)
+  def send(socket, msg) do
+    socket
+    |> elem(0)
+    |> case do
+      :sslsocket -> :ssl.send(socket, msg)
+      :gen_tcp -> :gen_tcp.send(socket, msg)
+    end
+  end
 
   @doc """
   Receive data from the socket
   """
-  def recv({:sslsocket, _, _} = socket, length), do: :ssl.recv(socket, length)
-  def recv({:gen_tcp, _, _, _} = socket, length), do: :gen_tcp.recv(socket, length)
-  def recv({:sslsocket, _, _} = socket, length, timeout), do: :ssl.recv(socket, length, timeout)
+  def recv(socket, length) do
+    socket
+    |> elem(0)
+    |> case do
+      :sslsocket -> :ssl.recv(socket, length)
+      :gen_tcp -> :gen_tcp.recv(socket, length)
+    end
+  end
 
-  def recv({:gen_tcp, _, _, _} = socket, length, timeout),
-    do: :gen_tcp.recv(socket, length, timeout)
+  def recv(socket, length, timeout) do
+    socket
+    |> elem(0)
+    |> case do
+      :sslsocket -> :ssl.recv(socket, length, timeout)
+      :gen_tcp -> :gen_tcp.recv(socket, length, timeout)
+    end
+  end
 end
