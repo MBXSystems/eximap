5a9bca084220fe6a8365515b0f997e52387ba03b
DJ Jain <dj.jain@ahead.com>
2025-10-26 09:42:26 -0500

    Update deps to latest versions
    New:
      file_system 1.1.1
      jason 1.4.4
    Unchanged:
      earmark_parser 1.4.44
      ex_doc 0.39.1
      makeup 1.2.1
      makeup_elixir 1.0.1
      makeup_erlang 1.0.2
      nimble_parsec 1.4.2
    Upgraded:
      bunt 0.2.1 => 1.0.0 (major)
      credo 0.8.10 => 1.7.13 (major)
      excoveralls 0.7.5 => 0.18.5 (minor)

--- mix.exs
+++ mix.exs
@@ -45,9 +45,9 @@ defmodule Eximap.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.8.8", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.7.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.39", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18.0", only: [:dev, :test], runtime: false}
     ]
   end
 
